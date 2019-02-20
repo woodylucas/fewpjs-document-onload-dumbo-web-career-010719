8d0c90cfec42107d7f6332a60b8868e204d7f328
woodylucas <wlucas0311@gmail.com>
2019-02-19 20:09:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+function domContentLoaded() {
+  document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+  updateDOM()
+  });
+}
+
+
+function updateDOM (){
+  document.querySelector('#text').textContent =  "This is really cool!"
+}
